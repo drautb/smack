be848c817f965e5126f701e4114ff83535db0fc1
Zvonimir <zvonimir@cs.utah.edu>
2017-02-08 13:52:46 -0700

    Running just corral

--- bin/build.sh
+++ bin/build.sh
@@ -403,7 +403,7 @@ then
   puts "Running SMACK regression tests"
 
   cd ${SMACK_DIR}/test
-  ./regtest.py --exhaustive --verifier=boogie
+  ./regtest.py --exhaustive --verifier=corral
   res=$?
 
   puts "Regression tests complete"
